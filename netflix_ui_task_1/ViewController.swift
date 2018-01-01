7d7f19c76eadfb266b1c69a6f16901263c166d54
satyam jha <jhasatyam386@gamil.com>
2018-01-01 09:43:34 +0530

    Initial Commit

---
+++ netflix_ui_task_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix_ui_task_1
+//
+//  Created by R&W on 01/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
